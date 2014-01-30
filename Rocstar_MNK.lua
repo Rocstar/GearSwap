f3a359bc6a70f7e16db114c86c8d063484f21035
Rocstar <john.east@techie.com>
2014-01-30 10:24:28 -0800

    Update Rocstar_MNK.lua

--- Rocstar_MNK.lua
+++ Rocstar_MNK.lua
@@ -10,17 +10,17 @@ sets.precast.JA['Hundred Fists'] = {legs="Melee hose +2"}
 sets.precast.JA.Chakra = {body="Anchorite's Cyclas",hands="Melee Gloves +2"}
 sets.precast.JA['Formless Strikes'] = {body="Melee cyclas +2"}
 sets.precast.JA['Chi Blast'] = {body="Melee crown +2"}
-sets.precast.JA.Step = {head="???"feet="???"}
-sets.precast.JA['Violent Flourish'] = {head="???"feet="???"}
+sets.precast.JA.Step = {head="???",feet="???"}
+sets.precast.JA['Violent Flourish'] = {head="???",feet="???"}
 sets.precast.JA.Waltz = {head="Anwig Salade",neck="Dualism Collar",ring1="Valseur's Ring",ring2="Veela Ring",
 waist="Aristo Belt",legs="Desultor Tassets",feet="Dance Shoes"}
 sets.precast.WS = {}
 --WS sets
-sets.precast.WS.Retribution = {head="???"feet="???"}
-sets.precast.WS['Victory Smite'] = {head="???"feet="???"}
-sets.precast.WS['Final Heaven'] = set_combine(sets.precast.WS['Victory Smite'], {head="???"feet="???"})
-sets.precast.WS['Ascetic\'s Fury'] = set_combine(sets.precast.WS['Victory Smite'], {head="???"feet="???"})
-sets.precast.WS['Shijin Spiral'] = set_combine(sets.precast.WS['Victory Smite'], {head="???"feet="???"})
+sets.precast.WS.Retribution = {head="???",feet="???"}
+sets.precast.WS['Victory Smite'] = {head="???",feet="???"}
+sets.precast.WS['Final Heaven'] = set_combine(sets.precast.WS['Victory Smite'], {head="???",feet="???"})
+sets.precast.WS['Ascetic\'s Fury'] = set_combine(sets.precast.WS['Victory Smite'], {head="???",feet="???"})
+sets.precast.WS['Shijin Spiral'] = set_combine(sets.precast.WS['Victory Smite'], {head="???",feet="???"})
 sets.aftercast = {}
 --Idle set
 sets.aftercast.Idle = {head="Oce. Headpiece",neck="Wiglen Gorget",body="Melee cyclas +2",
@@ -28,10 +28,10 @@ ring1="Paguroidea Ring",ring2="Sheltered Ring"}
 sets.aftercast.TP = sets.TP.DD
 sets.TP = {}
 --TP sets DD normal gear
-sets.TP.DD = {head="???"feet="???"}
-sets.TP.MD = {head="???"feet="???"}
-sets.TP.PD = {head="???"feet="???"}
-sets.TP.ACC = {head="???"feet="???"}
+sets.TP.DD = {head="???",feet="???"}
+sets.TP.MD = {head="???",feet="???"}
+sets.TP.PD = {head="???",feet="???"}
+sets.TP.ACC = {head="???",feet="???"}
 send_command('input /macro book 5;wait .1;input /macro set 1')
 end
 
