78292f0a74c213079edff7d9fca7e4a53b763a5b
Rocstar <john.east@techie.com>
2014-02-08 07:56:56 -0800

    Update Rocstar_WHM.lua

--- Rocstar_WHM.lua
+++ Rocstar_WHM.lua
@@ -1,98 +1,115 @@
 function get_sets()
 
--- precast sets =======================================================================
-	sets.precast = {}
-    sets.precast.EnhancingMagic = {waist="Siegel Sash"}
-	
-	sets.precast.JA = {}
-    sets.precast.JA.Benediction = {body="Cleric's briault +2"}
-    sets.precast.JA.Devotion = {body="Cleric's cap +2"}
-    sets.precast.JA.Martyr = {body="Cleric's mitts +2"}
-    sets.precast.JA['Elemental Seal'] = {main="Baqil Staff"}
-	
-	sets.precast.FC = {}
-	sets.precast.FC.Normal = {ammo="Impatiens",head="Nahtirah Hat",neck="Orunmila's Torque",
-	ear1="Loquacious Earring",body="Marduk's Jubbah +1",hands="Gendewitha Gages",ring1="Prolix Ring",
-	ring2="Veneficium Ring",back="Swith Cape +1",waist="Witful Belt",legs="Orvail Pants +1",feet="Chelona Boots +1"}
-		
-    sets.precast.FC.Cure = set_combine(sets.precast.FC.Light, {sub="Dominie's Grip",head="Cleric's cap +2",
-    	neck="Aceso's Choker",body="Heka's Kalasiris",back="Pahtli Cape",waist="Capricornian Rope",
-    	legs="Nabu's Shalwar",feet="Cure Clogs"})
-		
-    sets.precast.FC.Stoneskin = set_combine(sets.precast.EnhancingMagic, {hands="Carapacho Cuffs"})
-	
-    sets.precast.FC.Earth = {main="Vishrava I"}
-    sets.precast.FC.Fire = {main="Atar I"}
-    sets.precast.FC.Water = {main="Haoma I"}
-    sets.precast.FC.Wind = {main="Vayuvata I"}
-    sets.precast.FC.Ice = {main="Vourukasha I"}
-    sets.precast.FC.Thunder = {main="Apamajas I"}
-    sets.precast.FC.Light = {main="Arka I"}
-    sets.precast.FC.Dark = {main="Xsaeta I"}
+sets.JA = {}
+sets.JA.Benediction = {body="Cleric's briault +2"}
+sets.JA.Devotion = {body="Cleric's cap +2"}
+sets.JA.Martyr = {body="Cleric's mitts +2"}
+sets.JA['Elemental Seal'] = {main="Baqil Staff"}
 
-    sets.precast.WS = {}
-	sets.precast.WS.Retribution = {head="gear",feet="more gear"}
-	sets.precast.WS['Spirit Taker'] = {head="gear",feet="more gear"}
+sets.FC = {}
+sets.FC.EnhancingMagic = {waist="Siegel Sash"}
+
+sets.FC.Stoneskin = set_combine(sets.FC.EnhancingMagic, {hands="Carapacho Cuffs"})
 	
--- midcast sets ========================================================================
-    sets.midcast = {}
-	sets.midcast.Recast = {head="gear",feet="more gear"}
+sets.FC.Normal = {ammo="Impatiens",head="Nahtirah Hat",neck="Orunmila's Torque",
+ear1="Loquacious Earring",body="Marduk's Jubbah +1",hands="Gendewitha Gages",ring1="Prolix Ring",
+ring2="Veneficium Ring",back="Swith Cape +1",waist="Witful Belt",legs="Orvail Pants +1",feet="Chelona Boots +1"}
+
+sets.FC.Cure = set_combine(sets.FC.Light, {sub="Dominie's Grip",head="Cleric's cap +2",
+neck="Aceso's Choker",body="Heka's Kalasiris",back="Pahtli Cape",waist="Capricornian Rope",
+legs="Nabu's Shalwar",feet="Cure Clogs"})
+
+sets.FC.Earth = {main="Vishrava I"}
+sets.FC.Fire = {main="Atar I"}
+sets.FC.Water = {main="Haoma I"}
+sets.FC.Wind = {main="Vayuvata I"}
+sets.FC.Ice = {main="Vourukasha I"}
+sets.FC.Thunder = {main="Apamajas I"}
+sets.FC.Light = {main="Arka I"}
+sets.FC.Dark = {main="Xsaeta I"}
+
+sets.WeatherAndOrDay = {}
+sets.WeatherAndOrDay.Fire = {waist='Karin Obi',back='Twilight Cape',ring1='Zodiac Ring'}
+sets.WeatherAndOrDay.Earth = {waist='Dorin Obi',back='Twilight Cape',ring1='Zodiac Ring'}
+sets.WeatherAndOrDay.Water = {waist='Suirin Obi',back='Twilight Cape',ring1='Zodiac Ring'}
+sets.WeatherAndOrDay.Wind = {waist='Furin Obi',back='Twilight Cape',ring1='Zodiac Ring'}
+sets.WeatherAndOrDay.Ice = {waist='Hyorin Obi',back='Twilight Cape',ring1='Zodiac Ring'}
+sets.WeatherAndOrDay.Thunder = {waist='Rairin Obi',back='Twilight Cape',ring1='Zodiac Ring'}
+sets.WeatherAndOrDay.Light = {waist='Korin Obi',back='Twilight Cape'}
+sets.WeatherAndOrDay.Dark = {waist='Anrin Obi',back='Twilight Cape'}
+
+sets.WS = {}
+sets.WS.Retribution = {head="gear",feet="more gear"}
+sets.WS['Spirit Taker'] = {head="gear",feet="more gear"}
+
+sets.midcast = {}
+sets.midcast.Recast = {head="gear",feet="more gear"}
 	
-	sets.midcast.CurePotency = {main="Arka IV",sub="Verse Strap +1",head="Wayfarer Circlet",hands="Bokwus Gloves",
-	ring2="Sirona's Ring",legs="Wayfarer Slops"}
+sets.midcast.CurePotency = {main="Arka IV",sub="Verse Strap +1",head="Wayfarer Circlet",hands="Bokwus Gloves",
+ring2="Sirona's Ring",legs="Wayfarer Slops"}
+
+sets.midcast.na = {head="Orison Cap +2",legs="Orison Pantaloons +2"}
+
+sets.midcast.Regen = {body="Cleric's Briault +2",hands="Orison Mitts +2",legs="Theophany pantaloons"}
 	
-	sets.midcast.na = {head="Orison Cap +2",legs="Orison Pantaloons +2"}
+sets.midcast.Cursna = {main="Beneficus",sub="Genbu's Shield",head="Hyksos Khat",neck="Malison medallion",
+body="Orison Bliaud +2",hands="Hieros mittens",ring1="Ephedra ring", ring2="Ephedra ring",
+back="Mending cape",legs="Theophany pantaloons"}
 	
-	sets.midcast.Regen = {body="Cleric's Briault +2",hands="Orison Mitts +2",legs="Theophany pantaloons"}
+sets.midcast.Stoneskin = {hands="Gendewitha Gages",feet="Wayfarer Clogs"}
 	
-	sets.midcast.Cursna = {main="Beneficus",sub="Genbu's Shield",head="Hyksos Khat",neck="Malison medallion",
-	body="Orison Bliaud +2",hands="Hieros mittens",ring1="Ephedra ring", ring2="Ephedra ring",
-	back="Mending cape",legs="Theophany pantaloons"}
+sets.midcast.BarSolace = {main="Beneficus",head="Orison Cap +2",body="Orison Bliaud +2",hands="Orison Mitts +2",
+legs="Cleric's Pantaloons +2",feet="Orison Duckbills +2"}
 	
-	sets.midcast.Stoneskin = {hands="Gendewitha Gages",feet="Wayfarer Clogs"}
+sets.midcast.BarNoSolace = set_combine(sets.midcast.BarSolace,{body="Blessed Briault"})
 	
-	sets.midcast.BarSolace = {main="Beneficus",head="Orison Cap +2",body="Orison Bliaud +2",hands="Orison Mitts +2",
-	legs="Cleric's Pantaloons +2",feet="Orison Duckbills +2"}
+sets.midcast.Shell = {ring2="Sheltered Ring",legs="Cleric's pantaloons +2"}
 	
-	sets.midcast.BarNoSolace = set_combine(sets.midcast.BarSolace,{body="Blessed Briault"})
-	
-	sets.midcast.Shell = {ring2="Sheltered Ring",legs="Cleric's pantaloons +2"}
-	
-	sets.midcast.Pro = {ring2="Sheltered Ring",feet="Cleric's duckbills +2"}
-	
--- Aftercast Sets =======================================================================
-    sets.aftercast = {}
-    sets.aftercast.Idle = {main="Owleyes",sub="Genbu's Shield",ammo="Incantor Stone",head="Wivre Hairpin",
-	neck="Twilight Torque",ear1="Black Earring",ear2="Darkness Earring",body="Gendewitha Bliaut",
-	hands="Serpentes Cuffs",ring1="Dark Ring",ring2="Dark Ring",back="Cheviot Cape",
-	waist="Slipor Sash",legs="Tatsu. Sitagoromo",feet="Serpentes Sabots"}
-	
-    sets.aftercast.Engaged = {head="gear",feet="more gear"}
-	
-    sets.aftercast.HealingMP = set_combine(sets.aftercast.Idle, {head="gear",feet="more gear"})
-	
--- Weather and or Day Sets ============================================================
-    sets.WeatherAndOrDay = {}
-    sets.WeatherAndOrDay.Fire = {waist='Karin Obi',back='Twilight Cape',ring1='Zodiac Ring'}
-    sets.WeatherAndOrDay.Earth = {waist='Dorin Obi',back='Twilight Cape',ring1='Zodiac Ring'}
-    sets.WeatherAndOrDay.Water = {waist='Suirin Obi',back='Twilight Cape',ring1='Zodiac Ring'}
-    sets.WeatherAndOrDay.Wind = {waist='Furin Obi',back='Twilight Cape',ring1='Zodiac Ring'}
-    sets.WeatherAndOrDay.Ice = {waist='Hyorin Obi',back='Twilight Cape',ring1='Zodiac Ring'}
-    sets.WeatherAndOrDay.Thunder = {waist='Rairin Obi',back='Twilight Cape',ring1='Zodiac Ring'}
-    sets.WeatherAndOrDay.Light = {waist='Korin Obi',back='Twilight Cape'}
-    sets.WeatherAndOrDay.Dark = {waist='Anrin Obi',back='Twilight Cape'}
-    send_command('input /macro book 5;wait .1;input /macro set 1')
+sets.midcast.Pro = {ring2="Sheltered Ring",feet="Cleric's duckbills +2"}
+
+sets.TP = {}
+sets.TP.DD = {head="Twilight Helm"}
+sets.TP.Regen = {head="Twilight Helm"}
+sets.TP.Refresh = {head="Twilight Helm"}
+sets.TP['Magic Damage Taken -'] = {head="Twilight Helm"}
+sets.TP['Physical Damage Taken -'] = {head="Twilight Helm"}
+sets.TP['Breath Damage Taken -'] = {head="Twilight Helm"}
+sets.TP['Damage Taken -'] = {head="Twilight Helm"}
+sets.TP.ACC = {head="Twilight Helm"}
+
+sets.Idle = {}
+sets.Idle.Regen = {head="Twilight Helm"}
+sets.Idle.Refresh = {head="Twilight Helm"}
+sets.Idle['Magic Damage Taken -'] = {head="Twilight Helm"}
+sets.Idle['Physical Damage Taken -'] = {head="Twilight Helm"}
+sets.Idle['Breath Damage Taken -'] = {head="Twilight Helm"}
+sets.Idle['Damage Taken -'] = {head="Twilight Helm"}
+
+sets.aftercast = {}
+sets.aftercast.TP = sets.TP.DD
+sets.aftercast.Idle = sets.Idle.Refresh
+sets.aftercast.Resting = set_combine(sets.aftercast.Idle, {head="gear",feet="more gear"})
+
+send_command('@input /macro book 2;wait .1;input /macro set 1')
+send_command('@input /echo Idle Refresh mode, Engaged DD mode. ctrl = and ctrl - to change.')
+send_command('bind ^= gs c toggle engaged')
+send_command('bind ^- gs c toggle idle')
+    
 end
 
+function file_unload()
+send_command('unbind ^=')
+send_command('unbind ^-')end
+
 function precast(spell)
-if spell.action_type=='Magic'then equip(sets.precast.FC.Normal)
-if spell.english:startswith('Cur') and spell.name ~= 'Cursna' then equip(sets.precast.FC.Cure)
-if sets.precast.FC[spell.element]then equip(sets.precast.FC[spell.element]) 
-elseif spell.type=='EnhancingMagic'then equip(sets.precast.EnhancingMagic)
-elseif spell.english=='Stoneskin'then equip(sets.precast.FC.Stoneskin)
-elseif sets.precast.JA[spell.english]then equip(sets.precast.JA[spell.english])
+if spell.action_type=='Magic'then equip(sets.FC.Normal)
+if spell.english:startswith('Cur') and spell.name ~= 'Cursna' then equip(sets.FC.Cure)
+if sets.FC[spell.element]then equip(sets.FC[spell.element]) 
+elseif spell.type=='EnhancingMagic'then equip(sets.FC.EnhancingMagic)
+elseif spell.english=='Stoneskin'then equip(sets.FC.Stoneskin)
+elseif sets.JA[spell.english]then equip(sets.JA[spell.english])
 elseif spell.type=='WeaponSkill'then 
-if sets.precast.WS[spell.name]then equip(sets.precast.WS[spell.name])end end end end end
+if sets.WS[spell.name]then equip(sets.WS[spell.name])end end end end end
 
 function midcast(spell)
 if spell.english:startswith('Cure')or spell.english:startswith('Cura')then equip(sets.midcast.CurePotency)
@@ -110,14 +127,60 @@ elseif spell.english:startswith('Bar')then if buffactive['Afflatus Solace']
 then equip(sets.midcast.BarSolace)else equip(sets.midcast.BarNoSolace)end end end end
 
 function aftercast(spell)
-if player.status=='Engaged'then equip(sets.aftercast.Engaged)
+if player.status=='Engaged'then equip(sets.aftercast.TP)
 else equip(sets.aftercast.Idle)end end
 
 function status_change(new,old)
-if new=='Engaged'then equip(sets.aftercast.Engaged)disable('main','sub')
+if new=='Engaged'then equip(sets.aftercast.TP)disable('main','sub')
 elseif new=='Idle'then equip(sets.aftercast.Idle)
-elseif new=='Resting'then equip(sets.aftercast.HealingMP)enable('main','sub')end end
+elseif new=='Resting'then equip(sets.aftercast.Resting)enable('main','sub')end end
 
 function weathercheck(spell_element,set)
 if spell_element==world.weather_element or spell_element==world.day_element 
 then equip(set,sets['WeatherAndOrDay_'..spell_element])else equip(set)end end
+
+function self_command(command)
+if command=='toggle engaged'
+then if sets.aftercast.TP==sets.TP.DD 
+then sets.aftercast.TP=sets.TP.Regen 
+send_command('@input /echo Engaged set to Regen Mode')equip(sets.aftercast.TP)
+elseif sets.aftercast.TP==sets.TP.Regen 
+then sets.aftercast.TP=sets.TP.Refresh 
+send_command('@input /echo Engaged set to Refresh Mode')equip(sets.aftercast.TP)
+elseif sets.aftercast.TP==sets.TP.Refresh 
+then sets.aftercast.TP=sets.TP['Magic Damage Taken -'] 
+send_command('@input /echo Engaged set to Magic Damage Taken - Mode')equip(sets.aftercast.TP)
+elseif sets.aftercast.TP==sets.TP['Magic Damage Taken -'] 
+then sets.aftercast.TP=sets.TP['Physical Damage Taken -'] 
+send_command('@input /echo Engaged set to Physical Damage Taken - Mode')equip(sets.aftercast.TP)
+elseif sets.aftercast.TP==sets.TP['Physical Damage Taken -'] 
+then sets.aftercast.TP=sets.TP['Breath Damage Taken -'] 
+send_command('@input /echo Engaged set to Breath Damage Taken - Mode')equip(sets.aftercast.TP)
+elseif sets.aftercast.TP==sets.TP['Breath Damage Taken -'] 
+then sets.aftercast.TP=sets.TP['Damage Taken -'] 
+send_command('@input /echo Engaged set to Damage Taken - Mode')equip(sets.aftercast.TP)
+elseif sets.aftercast.TP==sets.TP['Damage Taken -'] 
+then sets.aftercast.TP=sets.TP.ACC 
+send_command('@input /echo Engaged set to Accuracy Mode')equip(sets.aftercast.TP)
+elseif sets.aftercast.TP==sets.TP.ACC 
+then sets.aftercast.TP=sets.TP.DD
+send_command('@input /echo Engaged set to Damage Dealer Mode')equip(sets.aftercast.TP)end
+elseif command=='toggle idle'
+then if sets.aftercast.Idle==sets.Idle.Regen 
+then sets.aftercast.Idle=sets.Idle.Refresh
+send_command('@input /echo Idle set to Refresh Mode')equip(sets.aftercast.Idledle)
+elseif sets.aftercast.Idle==sets.Idle.Refresh 
+then sets.aftercast.Idle=sets.Idle['Magic Damage Taken -'] 
+send_command('@input /echo Idle set to Magic Damage Taken - Mode')equip(sets.aftercast.Idledle)
+elseif sets.aftercast.Idle==sets.Idle['Magic Damage Taken -'] 
+then sets.aftercast.Idle=sets.Idle['Physical Damage Taken -'] 
+send_command('@input /echo Idle set to Physical Damage Taken - Mode')equip(sets.aftercast.Idledle)
+elseif sets.aftercast.Idle==sets.Idle['Physical Damage Taken -'] 
+then sets.aftercast.Idle=sets.Idle['Breath Damage Taken -'] 
+send_command('@input /echo Idle set to Breath Damage Taken - Mode')equip(sets.aftercast.Idledle)
+elseif sets.aftercast.Idle==sets.Idle['Breath Damage Taken -'] 
+then sets.aftercast.Idle=sets.Idle['Damage Taken -'] 
+send_command('@input /echo Idle set to Damage Taken - Mode')equip(sets.aftercast.Idledle)
+elseif sets.aftercast.Idle==sets.Idle['Damage Taken -'] 
+then sets.aftercast.Idle=sets.Idle.Regen 
+send_command('@input /echo Idle set to Regen  Mode')equip(sets.aftercast.idle)end end end
