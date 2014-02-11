216712508dff523e721e16bd2b1c69863802ac88
Rocstar <john.east@techie.com>
2014-02-10 20:20:34 -0800

    Update Rocstar_COR.lua

--- Rocstar_COR.lua
+++ Rocstar_COR.lua
@@ -1,13 +1,36 @@
-RangedAttackBullet='Adlivun Bullet'
-QuickDrawBullet='Animikii Bullet'
-PhysicalWeaponSkillBullet='Achiyalabopa Bullet'
-MagicWeaponSkillBullet='Bronze Bullet'
+--Ranged Attack Bullet
+RAB   = "Adlivun Bullet"
+
+--Quick Draw Bullet
+QDB   = "Titanium Bullet"
+
+--Physical Ranged Weapon Skill Bullet
+PRWSB = "Adlivun Bullet"
+
+--Magical Ranged Weapon Skill Bullet
+MRWSB = "Bronze Bullet"
+
+--Empty ammo to prevent firing Quick Draw Bullet
+X     = empty
 
 function get_sets()
 sets = {}
 
+-- Magical WS Set
+sets.MWS = {ear1="Friomisi Earring",ear2="Hecate's Earring"}
+
+-- Physical WS Set
+sets.PWS = {head="Uk'uxkaj Cap",neck="Asperity Necklace",ear1="Bladeborn Earring",ear2="Steelflash Earring"}
+
+-- Magical Ranged WS Set
+sets.MRWS = {ammo=MRWSB,ear1="Friomisi Earring",ear2="Hecate's Earring",back="Gunslinger's Cape"}
+
+-- Physical Ranged WS Set
+sets.PRWS = {ammo=PRWSB,head="Uk'uxkaj Cap",body="Navarch's Frac +2",hands="Navarch's Gants +2",ring1="Rajas Ring",
+ring2="Dark Ring",back="Gunslinger's Cape",legs="Kaabnax Trousers",feet="Iuitl Gaiters"}
+
 --Snapshot and Rapid Shot Set
-sets.Snapshot={ammo=RangedAttackBullet,head="Navarch's Tricorne +2",body="Laksamana's frac +1",
+sets.Snapshot={range="Donderbuss",ammo=RAB,head="Navarch's Tricorne +2",body="Laksamana's frac +1",
 hands="Iuitl Wristbands",back="Navarch's Mantle",waist="Impulse Belt",legs="Navarch's Culottes +2",
 feet="Wurrukatte Boots"}
 
@@ -15,15 +38,16 @@ feet="Wurrukatte Boots"}
 sets.RangedAttack = {head="Uk'uxkaj Cap",body="Navarch's Frac +2",hands="Navarch's Gants +2"}
 
 --Double-Up Set
---This ring likes to stay on while using Double-Up idk why...
 sets.Luz = {ring2="Luzaf's Ring"}
 
 --Phantom Roll Set
 sets.Roll = set_combine(sets.Luz, {head="Commodore's Tricorne +2",hands="Navarch's Gants +2"})
 
 --Quick Draw Set
-sets['Quick Draw'] = {ammo=QuickDrawBullet,head="Laksamana's Hat +1",feet="Laksamana's Bottes +1"}
+sets['Quick Draw'] = {range="Donderbuss",ammo=QDB,head="Laksamana's Hat +1",feet="Laksamana's Bottes +1"}
 
+sets['Fast Cast'] = {head="Haruspex Hat",neck="Orunmila's Torque",ear2="Loquacious Earring",
+hands="Thaumas Gloves",legs="Blood Cuisses"}
 
 -- Job Ability Sets
 sets.JA = {}
@@ -47,137 +71,124 @@ sets.JA['Tactician\'s Roll'] = set_combine(sets.Roll, {body="Navarch's Frac +2"}
 sets.JA['Allies\' Roll'] = set_combine(sets.Roll, {hands="Navarch's Gants +2"})
 
 -- Waltz Ability Delay -/TP Cost -/Potency +/VIT + Set
-sets.JA.Waltz = {back="Iximulew Cape"}
+sets.JA.Waltz = {head="Uk'uxkaj Cap",body="Wayfarer Robe",hands="Buremte Gloves",ring2="Dark Ring",
+back="Iximulew Cape",legs="Kaabnax Trousers",feet="Iuitl Gaiters"}
 
 sets.WS = {}
-
--- Magic Attack Bonus + WS Set
-sets.WS.Magical = {
-ear1="Friomisi Earring",ear2="Hecate's Earring"}
-
--- Critical Hit Damage +, Critical Hit Rate +, Double/Triple/Quadruple Attack WS Set
-sets.WS.Physical = {
-head="Uk'uxkaj Cap",neck="Asperity Necklace",ear1="Bladeborn Earring",ear2="Steelflash Earring"}
-
--- Ranged Magic Attack Bonus + WS Set
-sets.WS.RangedMagical = {ear1="Friomisi Earring",ear2="Hecate's Earring"}
-
--- Critical Hit Damage +, Critical Hit Rate +, Ranged Attack/Accuracy WS Set
-sets.WS.RangedPhysical = {hands="Manibozho Gloves"}
 
 -- WS Sets
 
 -- Exenterator 73% AGI mod on 1st merit, +3% for 2nd-5th merits. 85% AGI mod at 5/5.
-sets.WS.Exenterator = set_combine(sets.WS.Physical,{
-neck="Breeze Gorget",waist="Breeze Belt"})
+sets.WS.Exenterator = set_combine(sets.PWS,{neck="Breeze Gorget",waist="Breeze Belt"})
 
 -- Evisceration 30% DEX
-sets.WS.Evisceration = set_combine(sets.WS.Physical,{
-neck="Light Gorget",waist="Light Belt"})
+sets.WS.Evisceration = set_combine(sets.PWS,{neck="Light Gorget",waist="Light Belt"})
 
 -- Aeolian Edge 28% DEX / 28% INT
-sets.WS['Aeolian Edge'] = set_combine(sets.WS.Magical,{
-neck="Breeze Gorget",waist="Breeze Belt"})
+sets.WS['Aeolian Edge'] = set_combine(sets.MWS,{neck="Breeze Gorget",waist="Breeze Belt"})
 
 -- Requiescat 86% MND mod on 1st merit, +3.5% for 2nd-5th merits. 100% MND mod at 5/5.
-sets.WS.Requiescat = set_combine(sets.WS.Physical,{
-neck="Shadow Gorget",waist="Shadow Belt"})
+sets.WS.Requiescat = set_combine(sets.PWS,{neck="Shadow Gorget",waist="Shadow Belt"})
 
 -- Savage Blade 50% MND / 30% STR
-sets.WS['Savage Blade'] = set_combine(sets.WS.Physical,{
-neck="Breeze Gorget",waist="Breeze Belt"})
+sets.WS['Savage Blade'] = set_combine(sets.PWS,{neck="Breeze Gorget",waist="Breeze Belt"})
 
 -- Last Stand 73% AGI mod on 1st merit, +3% for 2nd-5th merits. 85% AGI mod at 5/5.
-sets.WS['Last Stand'] = set_combine(sets.WS.RangedPhysical,{
-ammo=PhysicalWeaponSkillBullet,neck="Light Gorget",waist="Light Belt"})
+sets.WS['Last Stand'] = set_combine(sets.PRWS,{neck="Light Gorget",waist="Light Belt"})
 
 -- Wildfire 60% AGI
-sets.WS.Wildfire = set_combine(sets.WS.RangedMagical,{
-ammo=MagicWeaponSkillBullet,neck="Shadow Gorget",waist="Shadow Belt"})
+sets.WS.Wildfire = set_combine(sets.MRWS,{neck="Shadow Gorget",waist="Shadow Belt"})
 
 -- Leaden Salute 30% AGI
-sets.WS['Leaden Salute'] = set_combine(sets.WS.RangedMagical,{
-ammo=MagicWeaponSkillBullet,neck="Shadow Gorget",waist="Shadow Belt"})
+sets.WS['Leaden Salute'] = set_combine(sets.MRWS,{neck="Shadow Gorget",waist="Shadow Belt"})
 
 -- Detonator 30% AGI
-sets.WS.Detonator = set_combine(sets.WS.RangedPhysical,{
-ammo=PhysicalWeaponSkillBullet,neck="Light Gorget",waist="Light Belt"})
+sets.WS.Detonator = set_combine(sets.PRWS,{neck="Light Gorget",waist="Light Belt"})
+
+-- Hot Shot 30% AGI
+sets.WS['Hot Shot'] = set_combine(sets.MRWS,{neck="Light Gorget",waist="Light Belt"})
+
+-- Slug Shot 30% AGI
+sets.WS['Slug Shot'] = set_combine(sets.PRWS,{neck="Light Gorget",waist="Light Belt"})
 
 sets.TP = {}
 
 -- Engaged Sets
 
 -- Engaged Damage Dealing Set
-sets.TP.DD = {ammo="empty",head="Uk'uxkaj Cap",neck="Asperity Necklace",ear1="Heartseeker Earring",
+sets.TP.DD = {ammo=X,head="Uk'uxkaj Cap",neck="Asperity Necklace",ear1="Heartseeker Earring",
 ear2="Dudgeon Earring",body="Manibozho Jerkin",hands="Buremte Gloves",ring1="Epona's Ring",
 ring2="Rajas Ring",back="Atheling Mantle",waist="Cetl Belt",
 legs="Quiahuiz Leggings",feet="Manibozho Boots"}
 
 -- Engaged Regen Set
-sets.TP.Reg = {ammo="empty",head="Ocelomeh Headpiece +1",neck="Wiglen Gorget",hands="Feronia's Bangles",
+sets.TP.Reg = {ammo=X,head="Ocelomeh Headpiece +1",neck="Wiglen Gorget",hands="Feronia's Bangles",
 ring1="Paguroidea Ring",ring2="Sheltered Ring",waist="Lycopodium Sash"}
 
 -- Engaged Refresh Set
-sets.TP.Ref = {ammo="empty",head="Wayfarer Circlet",body="Wayfarer Robe",hands="Wayfarer Cuffs",
+sets.TP.Ref = {ammo=X,head="Wayfarer Circlet",body="Wayfarer Robe",hands="Wayfarer Cuffs",
 legs="Wayfarer Slops",feet="Wayfarer Clogs"}
 
 -- Engaged Magic Damage Taken - / Magic Def. Bonus + Set
-sets.TP.MDT = {ammo="empty",head="Wayfarer Circlet",neck="Twilight Torque",body="Wayfarer Robe",
+sets.TP.MDT = {ammo=X,head="Wayfarer Circlet",neck="Twilight Torque",body="Wayfarer Robe",
 hands="Wayfarer Cuffs",ring1="Defending Ring",ring2="Dark Ring",back="Mollusca Mantle",
 legs="Wayfarer Slops",feet="Wayfarer Clogs"}
 
 -- Engaged Physical Damage Taken - 50% Set
-sets.TP.PDT = {ammo="empty",head="Iuitl Headgear",neck="Wiglen Gorget",ear1="Darkness Earring",
+sets.TP.PDT = {ammo=X,head="Iuitl Headgear",neck="Wiglen Gorget",ear1="Darkness Earring",
 ear2="Darkness Earring",body="Iuitl Vest",hands="Iuitl Wristbands",ring1="Defending Ring",
 ring2="Dark Ring",back="Iximulew Cape",legs="Iuitl Tights",feet="Iuitl Gaiters"}
 
 -- Engaged Breath Damage Taken - Set
-sets.TP.BDT = {ammo="empty",neck="Twilight Torque",ring1="Defending Ring",back="Mollusca Mantle"}
+sets.TP.BDT = {ammo=X,neck="Twilight Torque",ring1="Defending Ring",back="Mollusca Mantle"}
 
 -- Engaged Damage Taken - Set
-sets.TP.DT = {ammo="empty",neck="Twilight Torque",ring1="Defending Ring",back="Mollusca Mantle"}
+sets.TP.DT = {ammo=X,neck="Twilight Torque",ring1="Defending Ring",back="Mollusca Mantle"}
 
 -- Engaged Accuracy Set
-sets.TP.ACC = {ammo="empty",head="Manibozho Beret",body="Manibozho Jerkin",hands="Buremte Gloves",
-legs="Manibozho Brais",feet="Manibozho Boots"}
+sets.TP.ACC = {ammo="Honed Tathlum",head="Manibozho Beret",ear1="Heartseeker Earring",
+ear2="Steelflash Earring",body="Manibozho Jerkin",hands="Buremte Gloves",
+ring1="Ulthalam's Ring",legs="Manibozho Brais",feet="Manibozho Boots"}
 
 sets.Idle = {}
 
 -- Idle Sets
 
 -- Idle Regen Set
-sets.Idle.Reg = {head="Ocelomeh Headpiece +1",neck="Wiglen Gorget",hands="Feronia's Bangles",
-ring1="Paguroidea Ring",ring2="Sheltered Ring",waist="Lycopodium Sash"}
+sets.Idle.Reg = {ammo=X,head="Ocelomeh Headpiece +1",neck="Wiglen Gorget",ear1="Darkness Earring",
+ear2="Darkness Earring",body="Iuitl Vest",hands="Iuitl Wristbands",ring1="Paguroidea Ring",
+ring2="Sheltered Ring",back="Iximulew Cape",waist="Flume Belt",
+legs="Iuitl Tights",feet="Iuitl Gaiters"}
 
 -- Idle Refresh Set
-sets.Idle.Ref = {main="Terra's Staff",sub="Oneiros Grip",ammo="empty",head="Wayfarer Circlet",
+sets.Idle.Ref = {main="Terra's Staff",sub="Oneiros Grip",ammo=X,head="Wayfarer Circlet",
 neck="Wiglen Gorget",ear1="Darkness Earring",ear2="Darkness Earring",body="Wayfarer Robe",
 hands="Wayfarer Cuffs",ring1="Defending Ring",ring2="Dark Ring",back="Mollusca Mantle",
 waist="Flume Belt",legs="Wayfarer Slops",feet="Wayfarer Clogs"}
 
 -- Idle Magic Damage Taken - / Magic Def. Bonus + Set
-sets.Idle.MDT = {ammo="empty",neck="Twilight Torque",ring1="Defending Ring",
+sets.Idle.MDT = {ammo=X,neck="Twilight Torque",ring1="Defending Ring",
 ring2="Dark Ring",back="Mollusca Mantle"}
 
 -- Idle Physical Damage Taken - 50% Set
-sets.Idle.PDT = {ammo="empty",head="Iuitl Headgear",neck="Wiglen Gorget",ear1="Darkness Earring",
+sets.Idle.PDT = {ammo=X,head="Iuitl Headgear",neck="Wiglen Gorget",ear1="Darkness Earring",
 ear2="Darkness Earring",body="Iuitl Vest",hands="Iuitl Wristbands",ring1="Defending Ring",
 ring2="Dark Ring",back="Iximulew Cape",legs="Iuitl Tights",feet="Iuitl Gaiters"}
 
 -- Idle Breath Damage Taken - Set
-sets.Idle.BDT = {ammo="empty",neck="Twilight Torque",ring1="Defending Ring",back="Mollusca Mantle"}
+sets.Idle.BDT = {ammo=X,neck="Twilight Torque",ring1="Defending Ring",back="Mollusca Mantle"}
 
 -- Idle Damage Taken - Set
-sets.Idle.DT = {ammo="empty",neck="Twilight Torque",ring1="Defending Ring",back="Mollusca Mantle"}
+sets.Idle.DT = {ammo=X,neck="Twilight Torque",ring1="Defending Ring",back="Mollusca Mantle"}
 
 sets.aftercast = {}
 
 sets.aftercast.TP = sets.TP.DD
-sets.aftercast.Idle = sets.Idle.Ref
+sets.aftercast.Idle = sets.Idle.Reg
 
 --Command to set macro book
 --Key Binds when file loads  
-send_command('@input /macro book 1;wait .1;input /macro set 1')
+send_command('@input /macro book 19;wait .1;input /macro set 10')
 send_command('@input /echo Idle Refresh mode, Engaged DD mode. ctrl = and ctrl - to change.')
 send_command('bind ^= gs c toggle engaged')
 send_command('bind ^- gs c toggle idle')
@@ -190,14 +201,17 @@ send_command('unbind ^-')end
 
 function precast(spell)
 if sets.JA[spell.english]then equip(sets.JA[spell.english])
+elseif spell.action_type=='Magic'then equip(sets['Fast Cast'])
 elseif spell.action_type=='Ranged Attack'then equip(sets.Snapshot)
 elseif spell.english:endswith('Roll')then equip(sets.Roll)
 elseif spell.english:startswith('Dou')then equip(sets.Luz)
-elseif spell.english:endswith('Shot')then equip(sets['Quick Draw'])
 elseif spell.english=='Curing Waltz III'then equip(sets.JA.Waltz)
 elseif spell.english=='Spectral Jig'and buffactive.sneak then send_command('cancel 71')
-elseif spell.type=="WeaponSkill"then if sets.WS[spell.english]then equip(sets.WS[spell.english])end
-elseif spell.type=='Step'or spell.type=='Flourish1'then equip(sets.TP.AC)end end
+elseif spell.type=='CorsairShot'then equip(sets['Quick Draw'])
+elseif spell.type=="WeaponSkill"and sets.WS[spell.english]then equip(sets.WS[spell.english])
+elseif spell.type=='Step'or spell.type=='Flourish1'then equip(sets.TP.ACC)
+end end 
+
 
 function midcast(spell)
 if spell.action_type=='Ranged Attack'then equip(sets.RangedAttack)end
