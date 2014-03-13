414a2a07982e1ef7ed3c10a0ed47143cbe7b544d
Rocstar <john.east@techie.com>
2014-03-13 14:00:52 -0700

    Update Rocstar_THF.lua

--- Rocstar_THF.lua
+++ Rocstar_THF.lua
@@ -1,182 +1,171 @@
 function get_sets()
-sets.JA = {}
-sets.JA.Mug = {head="Assassin's bonnet +2"}
-sets.JA.Steal = {head="Assassin's bonnet +2",hands="Pillager's armlets +1",
-legs="Pillager's culottes +1",feet="Pillager's poulaines +1"}
-sets.JA['Perfect Dodge'] = {hands="Assassin's armlets +2"}
-sets.JA.Feint = {legs="Assassin's culottes +2"}
-sets.JA['Assassin\'s Charge'] = {feet="Assassin's poulaines +2"}
-sets.JA.Conspirator = {body="Raider's Vest +2"}
-sets.JA['Sneak Attack'] = {hands="Raider's armlets +2"}
-sets.JA.Collaborator = {head="Raider's Bonnet +2"}
-sets.JA.Accomplice = set_combine(sets.JA.Collaborator)
-sets.JA.Despoil = {legs="Raider's Culottes +2",feet="Raider's Poulaines +2"}
-sets.JA.Hide = {body="Pillager's vest +1"}
-sets.JA['Trick Attack'] = {hands="Pillager's armlets +1"}
-sets.JA.Flee = {feet="Pillager's poulaines +1"}
+add_to_chat(200, 'Gearswap: Engaged DD (ALT + F2 to change)')
+add_to_chat(200, 'Gearswap: Idle Regen (ALT + F3 to change)')
+add_to_chat(200, 'Gearswap: Auto Stun Disabled (ALT + F4 to change)')
+send_command('@input /macro book 4;wait .1;input /macro set 1')
+send_command('bind !F2 gs c E')
+send_command('bind !F3 gs c I')
+send_command('bind !F4 gs c A')
 
---VIT set for Curing Waltz
-sets.JA.Waltz = {head="Uk'uxkaj Cap",body="Iuitl Vest",hands="Buremte Gloves",
+JA={}WS={}WS.SA={}WS.TA={}WS.SATA={}TP={}Idle={}
+
+JA.Mug = {head="Assassin's Bonnet +2"}
+
+JA['Perfect Dodge'] = {hands="Plunderer's Armlets"}
+
+JA.Feint = {legs="Assassin's Culottes +2"}
+
+JA['Assassin\'s Charge'] = {feet="Assassin's Poulaines +2"}
+
+JA.Conspirator = {body="Raider's Vest +2"}
+
+JA['Sneak Attack'] = {hands="Raider's Armlets +2"}
+
+JA.Collaborator = {head="Raider's Bonnet +2"}
+
+JA.Accomplice = set_combine(JA.Collaborator)
+
+JA.Despoil = {legs="Raider's Culottes +2",feet="Raider's Poulaines +2"}
+
+JA.Hide = {body="Pillager's Vest +1"}
+
+JA['Trick Attack'] = {hands="Pillager's Armlets +1"}
+
+JA.Flee = {feet="Pillager's Poulaines +1"}
+
+JA['Curing Waltz III'] = JA['Curing Waltz']
+
+JA['Curing Waltz II'] = JA['Curing Waltz']
+
+JA['Curing Waltz'] = {head="Uk'uxkaj Cap",body="Iuitl Vest",hands="Buremte Gloves",
 back="Iximulew Cape",waist="Chuq'aba Belt",legs="Kaabnax Trousers",feet="Iuitl Gaiters"}
 
---WS sets
-sets.WS = {}
-sets.WS.SA = {}
-sets.WS.TA = {}
-sets.WS.SATA = {}
+JA.Steal = {head="Assassin's Bonnet +2",hands="Pillager's Armlets +1",
+legs="Pillager's Culottes +1",feet="Pillager's Poulaines +1"}
 
--- Exenterator 73% AGI mod on 1st merit, +3% for 2nd-5th merits. 85% AGI mod at 5/5.
-sets.WS.Exenterator = {head="Uk'uxkaj Cap",legs="Quiahuiz Leggings",feet="Asn. poulaines +2"}
-sets.WS.SA.Exenterator = set_combine(sets.WS.Exenterator,{hands="Raider's Armlets +2"})
-sets.WS.TA.Exenterator = set_combine(sets.WS.Exenterator,{hands="Iuitl Wristbands"})
-sets.WS.SATA.Exenterator = sets.WS.TA.Exenterator
+WS.Exenterator = {head="Uk'uxkaj Cap",legs="Quiahuiz Leggings",feet="Assassin's Poulaines +2"}
+WS.SA.Exenterator = set_combine(WS.Exenterator,{hands="Raider's Armlets +2"})
+WS.TA.Exenterator = set_combine(WS.Exenterator,{hands="Iuitl Wristbands"})
+WS.SATA.Exenterator = WS.TA.Exenterator
 
--- Rudra's Storm 60% DEX
-sets.WS['Rudra\'s Storm'] = {feet="Assassin's poulaines +2"}
-sets.WS.SA['Rudra\'s Storm'] = set_combine(sets.WS['Rudra\'s Storm'],{hands="Raider's Armlets +2"})
-sets.WS.TA['Rudra\'s Storm'] = set_combine(sets.WS['Rudra\'s Storm'],{hands="Pillager's Armlets +1"})
-sets.WS.SATA['Rudra\'s Storm'] = sets.WS.TA['Rudra\'s Storm']
+WS["Rudra's Storm"] = {feet="Assassin's Poulaines +2"}
+WS.SA["Rudra's Storm"] = set_combine(WS["Rudra's Storm"],{hands="Raider's Armlets +2"})
+WS.TA["Rudra's Storm"] = set_combine(WS["Rudra's Storm"],{hands="Pillager's Armlets +1"})
+WS.SATA["Rudra's Storm"] = WS.TA["Rudra's Storm"]
 
--- Evisceration 30% DEX
-sets.WS.Evisceration = {feet="Asn. poulaines +2"}
-sets.WS.SA.Evisceration = set_combine(sets.WS.Evisceration,{hands="Raider's Armlets +2"})
-sets.WS.TA.Evisceration = set_combine(sets.WS.Evisceration,{hands="Pillager's Armlets +1"})
-sets.WS.SATA.Evisceration = sets.WS.TA.Evisceration
+WS.Evisceration = {feet="Assassin's Poulaines +2"}
+WS.SA.Evisceration = set_combine(WS.Evisceration,{hands="Raider's Armlets +2"})
+WS.TA.Evisceration = set_combine(WS.Evisceration,{hands="Pillager's Armlets +1"})
+WS.SATA.Evisceration = WS.TA.Evisceration
 
--- Mandalic Stab 30% DEX
-sets.WS['Mandalic Stab'] = {feet="Asn. poulaines +2"}
-sets.WS.SA['Mandalic Stab'] = set_combine(sets.WS['Mandalic Stab'],{hands="Raider's Armlets +2"})
-sets.WS.TA['Mandalic Stab'] = set_combine(sets.WS['Mandalic Stab'],{hands="Pillager's Armlets +1"})
-sets.WS.SATA['Mandalic Stab'] = sets.WS.TA['Mandalic Stab']
+WS['Mandalic Stab'] = {feet="Assassin's Poulaines +2"}
+WS.SA['Mandalic Stab'] = set_combine(WS['Mandalic Stab'],{hands="Raider's Armlets +2"})
+WS.TA['Mandalic Stab'] = set_combine(WS['Mandalic Stab'],{hands="Pillager's Armlets +1"})
+WS.SATA['Mandalic Stab'] = WS.TA['Mandalic Stab']
 
--- Shark Bite 50% DEX
-sets.WS['Shark Bite'] = {feet="Asn. poulaines +2"}
-sets.WS.SA['Shark Bite'] = set_combine(sets.WS['Shark Bite'],{hands="Raider's Armlets +2"})
-sets.WS.TA['Shark Bite'] = set_combine(sets.WS['Shark Bite'],{hands="Pillager's Armlets +1"})
-sets.WS.SATA['Shark Bite'] = sets.WS.TA['Shark Bite']
+WS['Shark Bite'] = {feet="Assassin's Poulaines +2"}
+WS.SA['Shark Bite'] = set_combine(WS['Shark Bite'],{hands="Raider's Armlets +2"})
+WS.TA['Shark Bite'] = set_combine(WS['Shark Bite'],{hands="Pillager's Armlets +1"})
+WS.SATA['Shark Bite'] = WS.TA['Shark Bite']
 
--- Aeolian Edge 28% DEX / 28% INT
-sets.WS['Aeolian Edge'] = {feet="Asn. poulaines +2"}
-sets.WS.SA['Aeolian Edge'] = set_combine(sets.WS['Aeolian Edge'],{hands="Raider's Armlets +2"})
-sets.WS.TA['Aeolian Edge'] = set_combine(sets.WS['Aeolian Edge'],{hands="Pillager's Armlets +1"})
-sets.WS.SATA['Aeolian Edge'] = sets.WS.TA['Aeolian Edge']
+WS['Aeolian Edge'] = {feet="Assassin's Poulaines +2"}
+WS.SA['Aeolian Edge'] = set_combine(WS['Aeolian Edge'],{hands="Raider's Armlets +2"})
+WS.TA['Aeolian Edge'] = set_combine(WS['Aeolian Edge'],{hands="Pillager's Armlets +1"})
+WS.SATA['Aeolian Edge'] = WS.TA['Aeolian Edge']
 
--- Dancing Edge 40% CHR / 30% DEX
-sets.WS['Dancing Edge'] = {feet="Asn. poulaines +2"}
-sets.WS.SA['Dancing Edge'] = set_combine(sets.WS['Dancing Edge'],{hands="Raider's Armlets +2"})
-sets.WS.TA['Dancing Edge'] = set_combine(sets.WS['Dancing Edge'],{hands="Pillager's Armlets +1"})
-sets.WS.SATA['Dancing Edge'] = sets.WS.TA['Dancing Edge']
+WS['Dancing Edge'] = {feet="Assassin's Poulaines +2"}
+WS.SA['Dancing Edge'] = set_combine(WS['Dancing Edge'],{hands="Raider's Armlets +2"})
+WS.TA['Dancing Edge'] = set_combine(WS['Dancing Edge'],{hands="Pillager's Armlets +1"})
+WS.SATA['Dancing Edge'] = WS.TA['Dancing Edge']
 
--- Mercy Stroke 60% STR
-sets.WS['Mercy Stroke'] = {feet="Asn. poulaines +2"}
-sets.WS.SA['Mercy Stroke'] = set_combine(sets.WS['Mercy Stroke'],{hands="Raider's Armlets +2"})
-sets.WS.TA['Mercy Stroke'] = set_combine(sets.WS['Mercy Stroke'],{hands="Pillager's Armlets +1"})
-sets.WS.SATA['Mercy Stroke'] = sets.WS.TA['Mercy Stroke']
+WS['Mercy Stroke'] = {feet="Assassin's Poulaines +2"}
+WS.SA['Mercy Stroke'] = set_combine(WS['Mercy Stroke'],{hands="Raider's Armlets +2"})
+WS.TA['Mercy Stroke'] = set_combine(WS['Mercy Stroke'],{hands="Pillager's Armlets +1"})
+WS.SATA['Mercy Stroke'] = WS.TA['Mercy Stroke']
 
---Engaged sets
-sets.TP = {}
-
---Engaged Regen set
-sets.TP.RE = {head="Ocelomeh Headpiece +1",neck="Wiglen Gorget",ring1="Paguroidea Ring",ring2="Sheltered Ring"}
-
---Engaged DD Set
-sets.TP.DD = {main="Eminent Dagger",sub="Atoyac",range="Raider's Boomerang",head="Manibozho Beret",neck="Love Torque",
-ear1="Bladeborn Earring",ear2="Steelflash Earring",body="Manibozho Jerkin",hands="Buremte Gloves",
+TP.DD = {main="Eminent Dagger",sub="Atoyac",range="Raider's Boomerang",head="Uk'uxkaj Cap",neck="Love Torque",
+ear1="Bladeborn Earring",ear2="Steelflash Earring",body="Manibozho Jerkin",hands="Plunderer's Armlets",
 ring1="Epona's Ring",ring2="Cho'j Band",back="Atheling Mantle",waist="Twilight Belt",
-legs="Manibozho Brais",feet="Manibozho Boots"}
+legs="Quiahuiz Leggings",feet="Manibozho Boots"}
 
---Engaged Accuracy set
-sets.TP.AC = {head="Manibozho Beret",neck="Love Torque",body="Manibozho Jerkin",
+TP.ACC = {head="Manibozho Beret",neck="Love Torque",body="Manibozho Jerkin",
 hands="Buremte Gloves",waist="Dynamic Belt",legs="Manibozho Brais",feet="Manibozho Boots"}
 
---Engaged Magic Damage Taken - set
-sets.TP.MD = {head="Ejekamal Mask",neck="Twilight Torque",body="Iuitl Vest",
+TP.MDT = {head="Ejekamal Mask",neck="Twilight Torque",body="Iuitl Vest",
 hands="Buremte Gloves",back="Mollusca Mantle",legs="Kaabnax Trousers",feet="Iuitl Gaiters"}
 
---Engaged Physical Damage Taken - set
-sets.TP.PD = {neck="Wiglen Gorget",back="Mollusca Mantle"}
+TP.PDT = {neck="Wiglen Gorget",back="Mollusca Mantle"}
 
---Engaged Treasure Hunter set
-sets.TP.TH = set_combine(sets.TP.DD, {main="Eminent Dagger",sub="Thief's Knife",
-hands="Assassin's armlets +2",feet="Raider's Poulaines +2"})
+TP.DT = {neck="Twilight Torque",ring1="Defending Ring",ring2="Dark Ring",
+back="Mollusca Mantle",waist="Black Belt",legs="Otronif Brais",feet="Otronif Boots"}
 
---Idle sets
-sets.idle = {}
+TP.TH = set_combine(TP.DD, {main="Eminent Dagger",sub="Thief's Knife",
+hands="Plunderer's Armlets",feet="Raider's Poulaines +2"})
 
---Idle Regen set
-sets.idle.RE = {head="Ocelomeh Headpiece +1",neck="Wiglen Gorget",
-ring1="Paguroidea Ring",ring2="Sheltered Ring"}
+Idle.REG = {head="Ocelomeh Headpiece +1",neck="Wiglen Gorget",
+hands="Plunderer's Armlets",ring1="Paguroidea Ring",ring2="Sheltered Ring",feet="Skadi's Jambeaux"}
 
---Idle Treasure Hunter set
-sets.idle.TH = set_combine(sets.idle.RE, {main="Eminent Dagger",sub="Thief's Knife",
-hands="Assassin's armlets +2",feet="Raider's Poulaines +2"})
+Idle.PDT = {head="Ejekamal Mask",neck="Wiglen Gorget",body="Iuitl Vest",
+hands="Buremte Gloves",back="Mollusca Mantle",legs="Kaabnax Trousers",feet="Iuitl Gaiters"}
 
---Idle Physical Damage Taken - set
-sets.idle.PD = set_combine(sets.idle.RE, {head="Ejekamal Mask",neck="Wiglen Gorget",body="Iuitl Vest",
-hands="Buremte Gloves",back="Mollusca Mantle",legs="Kaabnax Trousers",feet="Iuitl Gaiters"})
+Idle.MDT = {head="Ejekamal Mask",neck="Twilight Torque",body="Iuitl Vest",
+hands="Buremte Gloves",back="Mollusca Mantle",legs="Kaabnax Trousers",feet="Iuitl Gaiters"}
 
---Idle Magic Damage Taken - set
-sets.idle.MD = set_combine(sets.idle.RE, {head="Ejekamal Mask",neck="Twilight Torque",body="Iuitl Vest",
-hands="Buremte Gloves",back="Mollusca Mantle",legs="Kaabnax Trousers",feet="Iuitl Gaiters"})
+Idle.DT = {neck="Twilight Torque",ring1="Defending Ring",ring2="Dark Ring",
+back="Mollusca Mantle",waist="Black Belt",legs="Otronif Brais",feet="Otronif Boots"}
 
---Aftercast Variables 
-sets.aftercast = {}
-sets.aftercast.TP = sets.TP.DD
-sets.aftercast.idle = sets.idle.RE
+----Engaged is a variable
+E = TP.DD
 
---Command to set macro book when reloading file
-send_command('@input /macro book 4;wait .1;input /macro set 1')
-send_command('@input /echo Idle Regen mode, Engaged DD mode. ctrl = and ctrl - to change.')
-send_command('bind ^= gs c toggle engaged')
-send_command('bind ^- gs c toggle idle')
-end
+----Idle is a variable
+I = Idle.REG
+
+----Auto Stun is a variable
+A = false
+
+windower.register_event('action', function(a)
+local m = windower.ffxi.get_mob_by_target('t')
+if A ~= false then if a.target_count ~= 0 then 
+if a.targets[1].action_count ~= 0 then
+if a.targets[1].actions[1].message ~= 0 then 
+if (m and m.is_npc and m.id == a.actor_id)and A:contains(a.category) then
+windower.send_command('input /ja "Violent Flourish" <t>')end end end end end end)end
 
 function precast(spell)
-if sets.JA[spell.english]then equip(sets.JA[spell.english])
-elseif spell.english == 'Spectral Jig'and buffactive.sneak then send_command('cancel 71')
-elseif spell.english:startswith('Cur')then equip(sets.JA.Waltz)
-elseif spell.type=='Step'or spell.type=='Flourish1'then equip(sets.TP.AC)
-elseif spell.type=='WeaponSkill'then if sets.WS[spell.name]then equip(sets.WS[spell.name])
-if buffactive['sneak attack']and buffactive['trick attack']and sets.WS.SATA[spell.english]
-then equip(sets.WS.SATA[spell.english])
-elseif buffactive['sneak attack'] and sets.WS.SA[spell.english] then equip(sets.WS.SA[spell.english])
-elseif buffactive['trick attack'] and sets.WS.TA[spell.english] then equip(sets.WS.TA[spell.english])end end end end
+if JA[spell.english]then equip(JA[spell.english])
+elseif spell.type=='Step'or spell.type=='Flourish1'then equip(TP.ACC)
+elseif spell.type=='WeaponSkill'then if WS[spell.english]then equip(WS[spell.english])
+elseif buffactive['sneak attack']and buffactive['trick attack']
+and WS.SATA[spell.english]then equip(WS.SATA[spell.english])
+elseif buffactive['sneak attack'] and WS.SA[spell.english] then equip(WS.SA[spell.english])
+elseif buffactive['trick attack'] and WS.TA[spell.english] then equip(WS.TA[spell.english])
+elseif spell.english=='Spectral Jig'and buffactive.sneak then send_command('cancel 71')end end end
 
 function aftercast(spell)
-if player.status=='Engaged'then equip(sets.aftercast.TP)
-else equip(sets.aftercast.idle)end end
+if player.status=='Engaged'then equip(E)
+else equip(I)end end
 
 function status_change(new,old)
-if new=='Idle'then equip(sets.aftercast.idle)
-elseif new=='Engaged'then equip(sets.aftercast.TP)
-end end
+if new=='Idle'then equip(I)
+elseif new=='Engaged'then equip(E)end end
 
 function self_command(command)
-if command=='toggle engaged'then 
-if sets.aftercast.TP==sets.TP.DD then sets.aftercast.TP=sets.TP.TH 
-send_command('@input /echo Engaged set to Treasure Hunter Mode')equip(sets.aftercast.TP)
-elseif sets.aftercast.TP==sets.TP.TH then sets.aftercast.TP=sets.TP.PD 
-send_command('@input /echo Engaged set to Physical Damage Taken - Mode')equip(sets.aftercast.TP)
-elseif sets.aftercast.TP==sets.TP.PD then sets.aftercast.TP=sets.TP.MD 
-send_command('@input /echo Engaged set to Magic Damage Taken - Mode')equip(sets.aftercast.TP)
-elseif sets.aftercast.TP==sets.TP.MD then sets.aftercast.TP=sets.TP.RE 
-send_command('@input /echo Engaged set to Regen Mode')equip(sets.aftercast.TP)
-elseif sets.aftercast.TP==sets.TP.RE then sets.aftercast.TP=sets.TP.AC 
-send_command('@input /echo Engaged set to Accuracy Mode')equip(sets.aftercast.TP)
-elseif sets.aftercast.TP==sets.TP.AC then sets.aftercast.TP=sets.TP.DD 
-send_command('@input /echo Engaged set to Damage Dealer Mode')equip(sets.aftercast.TP)end
-elseif command=='toggle idle'then
-if sets.aftercast.idle==sets.idle.RE then sets.aftercast.idle=sets.idle.TH
-send_command('@input /echo Idle set to Treasure Hunter Mode')equip(sets.aftercast.idle)
-elseif sets.aftercast.idle==sets.idle.TH then sets.aftercast.idle=sets.idle.PD 
-send_command('@input /echo Idle set to Physical Damage Taken - Mode')equip(sets.aftercast.idle)
-elseif sets.aftercast.idle==sets.idle.PD then sets.aftercast.idle=sets.idle.MD 
-send_command('@input /echo Idle set to Magic Damage Taken - Mode')equip(sets.aftercast.idle)
-elseif sets.aftercast.idle==sets.idle.MD then sets.aftercast.idle=sets.idle.RE 
-send_command('@input /echo Idle set to Regen Mode')equip(sets.aftercast.idle)end end  
-end
+    if command=='E'then 
+    if E==TP.DD then E=TP.TH add_to_chat(200, 'Gearswap: Engaged now TH')equip(E)
+elseif E==TP.TH then E=TP.PDT add_to_chat(200, 'Gearswap: Engaged now PDT -')equip(E)
+elseif E==TP.PDT then E=TP.MDT add_to_chat(200, 'Gearswap: Engaged now MDT -')equip(E)
+elseif E==TP.MDT then E=TP.DT add_to_chat(200, 'Gearswap: Engaged now DT -')equip(E)
+elseif E==TP.DT then E=TP.ACC add_to_chat(200, 'Gearswap: Engaged now Accuracy')equip(E)
+elseif E==TP.ACC then E=TP.DD add_to_chat(200, 'Gearswap: Engaged now DD')equip(E)end
 
-function file_unload()
-send_command('unbind ^=')
-send_command('unbind ^-')
-end
+elseif command=='I'then
+    if I==Idle.REG then I=Idle.PDT add_to_chat(200, 'Gearswap: Idle now PDT -')equip(I)
+elseif I==Idle.PDT then I=Idle.MDT add_to_chat(200, 'Gearswap: Idle now MDT -')equip(I)
+elseif I==Idle.MDT then I=Idle.DT add_to_chat(200, 'Gearswap: Idle now DT -')equip(I)
+elseif I==Idle.DT then I=Idle.REG add_to_chat(200, 'Gearswap: Idle now Regen')equip(I)end 
+
+elseif command=='A'then 
+    if A==false then A=S{7,8} add_to_chat(200, 'Gearswap: Auto Stun now TP and Spellcasting')
+elseif A==S{7,8} then A=S{7} add_to_chat(200, 'Gearswap: Auto Stun now TP')
+elseif A==S{7} then A=S{8} add_to_chat(200, 'Gearswap: Auto Stun now Spellcasting')
+elseif A==S{8} then A=false add_to_chat(200, 'Gearswap: Auto Stun now Disabled')end end end
