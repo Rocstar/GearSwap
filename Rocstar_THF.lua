ef0cd0cc2c59c136a396e98aca49e59f69d2b5b8
Rocstar <john.east@techie.com>
2014-04-14 08:46:37 -0700

    Update Rocstar_THF.lua

--- Rocstar_THF.lua
+++ Rocstar_THF.lua
@@ -1,11 +1,11 @@
-function get_sets() full_time = {} none = {} pre = {} pre.SA = {} pre.TA = {} pre.SATA = {} 
+function get_sets() pre = {} 
 
-  add_to_chat(200, 'Gearswap: Auto Stun Disabled (ALT F9) TH Mode Tag (F9)') 
-
-  send_command('@input /macro book 4;wait .1;input /macro set 1') 
+  add_to_chat(200, 'Gearswap: Auto Stun Disabled (ALT F9)') 
+  add_to_chat(200, 'Gearswap: TH Tag with Steps and Violent Flourish (F9)') 
+  send_command('@input /macro book 6;wait .1;input /macro set 1') 
 
 --Key Binds
-  send_command('bind f9 gs c TH_Mode') 
+  send_command('bind f9 gs c TH_mode') 
   send_command('bind !f9 gs c Auto_Stun') 
   
 --Job Ability
@@ -19,76 +19,53 @@ function get_sets() full_time = {} none = {} pre = {} pre.SA = {} pre.TA = {} pr
   pre.Despoil = {legs="Raider's Culottes +2",feet="Raider's Poulaines +2"} 
   pre.Hide = {body="Pillager's Vest +1"} 
   pre.Flee = {feet="Pillager's Poulaines +1"} 
-  pre['Curing Waltz III'] = pre['Curing Waltz'] 
-  pre['Curing Waltz II'] = pre['Curing Waltz'] 
   pre['Curing Waltz'] = {head="Uk'uxkaj Cap",body="Iuitl Vest",
     hands="Buremte Gloves",back="Iximulew Cape",
     waist="Chuq'aba Belt",legs="Kaabnax Trousers",
     feet="Iuitl Gaiters"} 
+  pre['Curing Waltz II'] = set_combine(pre['Curing Waltz']) 
+  pre['Curing Waltz III'] = set_combine(pre['Curing Waltz']) 
   pre.Steal = {head="Assassin's Bonnet +2",hands="Pillager's Armlets +1",
     legs="Pillager's Culottes +1",feet="Pillager's Poulaines +1"} 
-  pre['Sneak Attack'] = {hands="Raider's Armlets +2"} 
-  pre['Trick Attack'] = {hands="Pillager's Armlets +1"} 
   	
---Weapon Skills
-  pre.Exenterator = {head="Uk'uxkaj Cap",legs="Quiahuiz Leggings",feet="Assassin's Poulaines +2"} 
-  pre.SA.Exenterator = set_combine(pre.Exenterator, {hands="Raider's Armlets +2"}) 
-  pre.TA.Exenterator = set_combine(pre.Exenterator, {hands="Iuitl Wristbands"}) 
-  pre.SATA.Exenterator = pre.TA.Exenterator 
+-- Dagger Weapon Skills
+  pre.Exenterator = {neck="Breeze Gorget",waist="Breeze Belt"} 
 
-  pre["Rudra's Storm"] = {feet="Assassin's Poulaines +2"} 
-  pre.SA["Rudra's Storm"] = set_combine(pre["Rudra's Storm"], {hands="Raider's Armlets +2"}) 
-  pre.TA["Rudra's Storm"] = set_combine(pre["Rudra's Storm"], {hands="Pillager's Armlets +1"}) 
-  pre.SATA["Rudra's Storm"] = pre.TA["Rudra's Storm"] 
+  pre["Rudra's Storm"] = {neck="Shadow Gorget",waist="Shadow Belt"} 
 
-  pre.Evisceration = {feet="Assassin's Poulaines +2"} 
-  pre.SA.Evisceration = set_combine(pre.Evisceration, {hands="Raider's Armlets +2"}) 
-  pre.TA.Evisceration = set_combine(pre.Evisceration, {hands="Pillager's Armlets +1"}) 
-  pre.SATA.Evisceration = pre.TA.Evisceration 
+  pre.Evisceration = {neck="Shadow Gorget",waist="Shadow Belt"} 
 
-  pre['Mandalic Stab'] = {feet="Assassin's Poulaines +2"} 
-  pre.SA['Mandalic Stab'] = set_combine(pre['Mandalic Stab'], {hands="Raider's Armlets +2"}) 
-  pre.TA['Mandalic Stab'] = set_combine(pre['Mandalic Stab'], {hands="Pillager's Armlets +1"}) 
-  pre.SATA['Mandalic Stab'] = pre.TA['Mandalic Stab'] 
+  pre['Mandalic Stab'] = {neck="Shadow Gorget",waist="Shadow Belt"} 
 
-  pre['Shark Bite'] = {feet="Assassin's Poulaines +2"} 
-  pre.SA['Shark Bite'] = set_combine(pre['Shark Bite'], {hands="Raider's Armlets +2"}) 
-  pre.TA['Shark Bite'] = set_combine(pre['Shark Bite'], {hands="Pillager's Armlets +1"}) 
-  pre.SATA['Shark Bite'] = pre.TA['Shark Bite'] 
-
-  pre['Aeolian Edge'] = {feet="Assassin's Poulaines +2"} 
-  pre.SA['Aeolian Edge'] = set_combine(pre['Aeolian Edge'], {hands="Raider's Armlets +2"}) 
-  pre.TA['Aeolian Edge'] = set_combine(pre['Aeolian Edge'], {hands="Pillager's Armlets +1"}) 
-  pre.SATA['Aeolian Edge'] = pre.TA['Aeolian Edge'] 
-
-  pre['Dancing Edge'] = {feet="Assassin's Poulaines +2"} 
-  pre.SA['Dancing Edge'] = set_combine(pre['Dancing Edge'], {hands="Raider's Armlets +2"}) 
-  pre.TA['Dancing Edge'] = set_combine(pre['Dancing Edge'], {hands="Pillager's Armlets +1"}) 
-  pre.SATA['Dancing Edge'] = pre.TA['Dancing Edge'] 
-
-  pre['Mercy Stroke'] = {feet="Assassin's Poulaines +2"} 
-  pre.SA['Mercy Stroke'] = set_combine(pre['Mercy Stroke'], {hands="Raider's Armlets +2"}) 
-  pre.TA['Mercy Stroke'] = set_combine(pre['Mercy Stroke'], {hands="Pillager's Armlets +1"}) 
-  pre.SATA['Mercy Stroke'] = pre.TA['Mercy Stroke'] 
+  pre['Shark Bite'] = {neck="Breeze Gorget",waist="Breeze Belt"} 
   
--- Variables for Engaged Idle TH and Auto Stun Modes
-  DD = {main="Sandung",sub="Eminent Dagger",
-    range="Raider's Boomerang",head="Uk'uxkaj Cap",
-    neck="Love Torque",ear1="Bladeborn Earring",
+  pre['Aeolian Edge'] = {neck="Breeze Gorget",waist="Breeze Belt"} 
+
+  pre['Dancing Edge'] = {neck="Breeze Gorget",waist="Breeze Belt"} 
+
+  pre['Mercy Stroke'] = {neck="Shadow Gorget",waist="Shadow Belt"} 
+  
+-- Variables 
+  DD = {main="Izhiikoh",sub="Sandung",
+    range="Raider's Boomerang",head="Manibozho Beret",
+    neck="Asperity Necklace",ear1="Bladeborn Earring",
     ear2="Steelflash Earring",body="Manibozho Jerkin",
-    hands="Buremte Gloves",ring1="Epona's Ring",
-    ring2="Cho'j Band",back="Atheling Mantle",
+    hands="Buremte Gloves",ring1="Rajas Ring",
+	ring2="Epona's Ring",back="Atheling Mantle",
     waist="Cetl Belt",legs="Manibozho Brais",
     feet="Manibozho Boots"} 
 
   ACC = {head="Manibozho Beret",neck="Love Torque",
     body="Manibozho Jerkin",hands="Buremte Gloves",
-    waist="Dynamic Belt",legs="Manibozho Brais",
-    feet="Manibozho Boots"} 
+	back="Canny Cape",waist="Dynamic Belt",
+	legs="Manibozho Brais",feet="Manibozho Boots"} 
  
-  REG = {head="Ocelomeh Headpiece +1",neck="Wiglen Gorget",
-    hands="Plunderer's Armlets",ring1="Paguroidea Ring",
-    ring2="Sheltered Ring",feet="Skadi's Jambeaux"} 
+  REG = {neck="Wiglen Gorget",ring1="Paguroidea Ring",
+    ring2="Sheltered Ring",back="Iximulew Cape"} 
+
+  Extra_Regen = {head="Ocelomeh Headpiece +1",neck="Wiglen Gorget",
+    ring1="Paguroidea Ring",ring2="Sheltered Ring",
+	back="Iximulew Cape"} 
 
   PDT = {head="Ejekamal Mask",neck="Wiglen Gorget",
     body="Iuitl Vest",hands="Buremte Gloves",
@@ -104,37 +81,31 @@ function get_sets() full_time = {} none = {} pre = {} pre.SA = {} pre.TA = {} pr
     ring2="Dark Ring",back="Mollusca Mantle",
     waist="Black Belt",legs="Otronif Brais",
     feet="Otronif Boots"} 
-
-  TH8 = set_combine(DD, {hands="Plunderer's Armlets",waist="Chaac Belt",
-    feet="Raider's Poulaines +2"}) 
- 	
-  TH10 = set_combine(DD, {main="Sandung",sub="Thief's Knife",
-    hands="Plunderer's Armlets",waist="Chaac Belt",feet="Raider's Poulaines +2"}) 
 	
-  E = DD 
+  TH_tag ={hands="Plunderer's Armlets",feet="Raider's Poulaines +2"} 
+ 	
+  TH_full = set_combine(DD, {main="Sandung",sub="Thief's Knife",
+    hands="Plunderer's Armlets",waist="Chaac Belt",feet="Raider's Poulaines +2"}) 
 
-  I = REG 
+  SA = {head="Uk'uxkaj Cap",hands="Raider's Armlets +2",
+    feet="Iuitl Gaiters"} 
+	
+  TA = {head="Lithelimb Cap",hands="Pillager's Armlets +1",
+    back="Canny Cape",legs="Kaabnax Trousers",
+    feet="Iuitl Gaiters"}
+	
+  SATA = set_combine(TA, {}) 
+  
+  Weaponskill = DD 
+    
+  Engaged = DD 
 
+  Idle = REG 
+  
+  TH_mode = TH_tag 
+  
   Auto_Stun = false 
-
-  TH_mode = false 
-
---Auto Stun Mob Actions
-  windower.register_event('action', function(Action_Stun) 
-    if Auto_Stun ~= false then 
-     local mob=windower.ffxi.get_mob_by_target('t')
-      if Action_Stun.target_count ~= 0 then 
-        if Action_Stun.targets[1].action_count ~= 0 then 
-          if Action_Stun.targets[1].actions[1].message ~= 0 then 
-            if (mob and mob.is_npc and mob.id == Action_Stun.actor_id) and 
-              Auto_Stun:contains(Action_Stun.category) then 
-              windower.send_command('input /ja "Violent Flourish" <t>') 
-            end 
-          end 
-        end 
-      end 
-    end 
-  end) 
+  
 end
 
 function file_unload() 
@@ -143,59 +114,75 @@ function file_unload()
 end 
 
 function precast(spell) 
-  if spell.type ~= 'WeaponSkill' and pre[spell.english] then 
-    equip(pre[spell.english]) 
-  elseif buffactive['sneak attack'] and pre.SA[spell.english] then 
-    equip(pre.SA[spell.english]) 
-  elseif buffactive['trick attack'] and pre.TA[spell.english] then 
-    equip(pre.TA[spell.english]) 
-  elseif buffactive['sneak attack'] and buffactive['trick attack'] and pre.SATA[spell.english] then 
-    equip(pre.SATA[spell.english]) 
-  elseif spell.type == 'WeaponSkill' and pre[spell.english] then 
-    equip(pre[spell.english]) 
+  if pre[spell.english] then 
+    if spell.type == "WeaponSkill" then 
+      equip(Weaponskill, pre[spell.english]) 
+	else 
+	  equip(pre[spell.english]) 
+	end 
+  elseif S{'Step','Flourish1'}:contains(spell.type) then 
+    if TH_mode == TH_tag then 
+      equip(TH_tag) 
+    else 
+	  equip(ACC) 
+	end
   elseif spell.english == 'Spectral Jig' and buffactive.sneak then 
     send_command('cancel 71') 
-  elseif spell.type == 'Step'or spell.type == 'Flourish1' then 
-    equip(ACC) 
   end 
 end 
 
 function aftercast(spell) 
   if player.status == 'Engaged' then 
-    equip(E) 
+    equip(Engaged) 
   else 
-    equip(I) 
+    equip(Idle) 
   end 
 end 
 
 function status_change(new,old) 
   if new == 'Engaged' then 
-    if TH_mode ~= full_time and TH_mode ~= none then 
-      TH_mode = tag 
-      equip(TH8) 
-        windower.register_event('tp change', function(new, old) 
-          if new and TH_mode == tag then 
-            equip(E) 
-            TH_mode = false 
-          end 
-        end) 
-    else 
-      equip(E) 
-    end
-  else
-    equip(I) 
-  end
+    equip(Engaged) 
+  else 
+    equip(Idle) 
+  end 
 end 
 
-function buff_change(buff, gain)
+function buff_change(buff, gain) 
+local b = player.status
   if buff == 'Feint' and gain then 
     equip(pre.Feint) 
   elseif buff == 'Sneak Attack' and gain then 
-    equip(pre['Sneak Attack']) 
+    equip(SA) 
+	Weaponskill = SA 
   elseif buff == 'Trick Attack' and gain then 
-    equip(pre['Trick Attack']) 
-  else 
-    equip(E) 
+    equip(TA) 
+	Weaponskill = TA 
+  elseif buff == 'Trick Attack' and gain and buffactive['Sneak attack'] then 
+    equip(SATA) 
+	Weaponskill = SATA 
+  elseif buff == 'Sneak Attack' and gain and buffactive['Trick Attack'] then 
+    equip(SATA) 
+	Weaponskill = SATA
+  elseif buff == 'Feint' and not gain then 
+    if b == 'Engaged' then 
+      equip(Engaged) 
+	else 
+	  equip(Idle)
+	end
+  elseif buff == 'Sneak Attack' and not gain then 
+    Weaponskill = DD 
+    if b == 'Engaged' then 
+      equip(Engaged) 
+	else 
+	  equip(Idle)
+	end
+  elseif buff == 'Trick Attack' and not gain then 
+    Weaponskill = DD 
+    if b == 'Engaged' then 
+      equip(Engaged) 
+	else 
+	  equip(Idle)
+	end
   end 
 end 
 
@@ -213,19 +200,43 @@ function self_command(command)
     elseif Auto_Stun == S{8} then 
       Auto_Stun = false 
       add_to_chat(200, 'Gearswap: Auto Stun now Disabled') 
-    end 
-  elseif command == 'TH_Mode' then 
-    if TH_mode == false or TH_mode == none  then 
-      TH_mode = tag 
-      add_to_chat(200, 'Gearswap: TH now Tag') 
-    elseif TH_mode == tag then 
-      TH_mode = full_time 
-      E = TH10 
+	end
+  elseif command == 'TH_mode' then 
+    if S{false, nil}:contains(TH_mode) then 
+      TH_mode = TH_tag
+      add_to_chat(200, 'Gearswap: TH Tag with Steps and Violent Flourish') 
+    elseif TH_mode == TH_tag then 
+      TH_mode = full
+      Engaged = TH_full 
       add_to_chat(200, 'Gearswap: TH now Full Time') 
-    elseif TH_mode == full_time then 
-      TH_mode = none 
-      E = DD 
+    elseif TH_mode == full then 
+      TH_mode = false
+      Engaged = DD 
       add_to_chat(200, 'Gearswap: TH now Disabled') 
     end 
   end 
 end 
+
+windower.register_event('hpp change', function(new, old) 
+  if new <= 96 then 
+    Idle = Extra_Regen 
+  else 
+    Idle = REG 
+  end 
+end) 
+
+windower.register_event('action', function(Action_Stun) 
+  if Auto_Stun ~= false then 
+    local mob=windower.ffxi.get_mob_by_target('t')
+    if Action_Stun.target_count ~= 0 then 
+      if Action_Stun.targets[1].action_count ~= 0 then 
+        if Action_Stun.targets[1].actions[1].message ~= 0 then 
+          if (mob and mob.is_npc and mob.id == Action_Stun.actor_id) and 
+            Auto_Stun:contains(Action_Stun.category) then 
+            windower.send_command('input /ja "Violent Flourish" <t>') 
+          end 
+        end 
+      end 
+    end 
+  end 
+end) 
