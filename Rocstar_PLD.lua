53b76cbb1fb82bf083be1ad32140c4066bea0f23
Rocstar <john.east@techie.com>
2014-04-21 00:27:04 -0700

    Update Rocstar_PLD.lua

--- Rocstar_PLD.lua
+++ Rocstar_PLD.lua
@@ -29,10 +29,10 @@ function get_sets() pre = {}
   pre['Curing Waltz'] = {ammo="",head="",
     neck="",ear1="",
     ear2="",body="",
-	hands="",ring1="",
-	ring2="",back="",
-	waist="",legs="",
-	feet=""} 
+    hands="",ring1="",
+    ring2="",back="",
+    waist="",legs="",
+    feet=""} 
 
   pre['Curing Waltz II'] = pre['Curing Waltz'] 
 
@@ -43,10 +43,10 @@ function get_sets() pre = {}
   pre.cast = {ammo="",head="",
     neck="",ear1="",
     ear2="",body="",
-	hands="",ring1="",
-	ring2="",back="",
-	waist="",legs="",
-	feet=""} 
+    hands="",ring1="",
+    ring2="",back="",
+    waist="",legs="",
+    feet=""} 
 	
 --[[ Weapon SkillS ]]--
 
@@ -65,66 +65,66 @@ function get_sets() pre = {}
   ACC = {ammo="",head="",
     neck="",ear1="",
     ear2="",body="",
-	hands="",ring1="",
-	ring2="",back="",
-	waist="",legs="",
-	feet=""} 
+    hands="",ring1="",
+    ring2="",back="",
+    waist="",legs="",
+    feet=""} 
  
   DD = {ammo="",head="",
     neck="",ear1="",
     ear2="",body="",
-	hands="",ring1="",
-	ring2="",back="",
-	waist="",legs="",
-	feet=""} 
+    hands="",ring1="",
+    ring2="",back="",
+    waist="",legs="",
+    feet=""} 
 	
   MDT = {ammo="",head="",
     neck="",ear1="",
     ear2="",body="",
-	hands="",ring1="",
-	ring2="",back="",
-	waist="",legs="",
-	feet=""} 
+    hands="",ring1="",
+    ring2="",back="",
+    waist="",legs="",
+    feet=""} 
 
   PDT = {ammo="",head="",
     neck="",ear1="",
     ear2="",body="",
-	hands="",ring1="",
-	ring2="",back="",
-	waist="",legs="",
-	feet=""} 
+    hands="",ring1="",
+    ring2="",back="",
+    waist="",legs="",
+    feet=""} 
 
   BDT = {ammo="",head="",
     neck="",ear1="",
     ear2="",body="",
-	hands="",ring1="",
-	ring2="",back="",
-	waist="",legs="",
-	feet=""} 
+    hands="",ring1="",
+    ring2="",back="",
+    waist="",legs="",
+    feet=""} 
 
   DT = {ammo="",head="",
     neck="",ear1="",
     ear2="",body="",
-	hands="",ring1="",
-	ring2="",back="",
-	waist="",legs="",
-	feet=""} 
+    hands="",ring1="",
+    ring2="",back="",
+    waist="",legs="",
+    feet=""} 
   
   Aftermath = {ammo="",head="",
     neck="",ear1="",
     ear2="",body="",
-	hands="",ring1="",
-	ring2="",back="",
-	waist="",legs="",
-	feet=""} 
+    hands="",ring1="",
+    ring2="",back="",
+    waist="",legs="",
+    feet=""} 
 	
   Regen = {ammo="",head="",
     neck="",ear1="",
     ear2="",body="",
-	hands="",ring1="",
-	ring2="",back="",
-	waist="",legs="",
-	feet=""} 
+    hands="",ring1="",
+    ring2="",back="",
+    waist="",legs="",
+    feet=""} 
 
   weapon_skill = ACC 
   
@@ -140,7 +140,7 @@ function precast(spell)
   elseif spell.type == 'WeaponSkill' then 
     if pre[spell.english] then 
       equip(weapon_skill, pre[spell.english]) 
-	end
+    end
   else 
     equip(pre[spell.english]) 
   end 
@@ -165,21 +165,21 @@ end
 function buff_change(buff, gain) 
   if buff == 'Divine Emblem' then 
     if gain then 
-	  equip({feet="Creed Sabatons +2"}) 
-	    if player.equipment.feet == "Creed Sabatons +2" then 
-	      disable('feet') 
-		end
+      equip({feet="Creed Sabatons +2"}) 
+        if player.equipment.feet == "Creed Sabatons +2" then 
+          disable('feet') 
+        end
     else 
-	  enable('feet') 
-	end
+      enable('feet') 
+    end
   elseif buff == 'Aftermath: Lv.3' then 
     if gain then 
-	  engaged = Aftermath
-	  equip(engaged) 
-	else 
-	  engaged = PDT
-	  equip(engaged) 
-	end
+      engaged = Aftermath
+      equip(engaged) 
+    else 
+      engaged = PDT
+      equip(engaged) 
+    end
   end 
 end
 
@@ -188,53 +188,53 @@ function self_command(command)
 --[[ Toggle Weapon Skill ACC ( CTRL F9 ) or ( //gs c w ) ]]--	
   if command == 'w' then 
     if weapon_skill == ACC then 
-	  weapon_skill = DD 
-	elseif weapon_skill == DD then 
-	  weapon_skill = ACC 
+      weapon_skill = DD 
+    elseif weapon_skill == DD then 
+      weapon_skill = ACC 
     end 
 	
 --[[ Toggle Idle ( ALT F9 ) or ( //gs c i ) ]]--		
   elseif command == 'i' then 
     if idle == PDT then 
-	  idle = MDT 
-	    if player.status ~= 'Engaged' then 
-		  equip(idle) 
-		end 
-	elseif idle == MDT then 
-	  idle = PDT 
-	    if player.status ~= 'Engaged' then 
-		  equip(idle) 
-		end 
+      idle = MDT 
+        if player.status ~= 'Engaged' then 
+          equip(idle) 
 	end 
+    elseif idle == MDT then 
+      idle = PDT 
+        if player.status ~= 'Engaged' then 
+          equip(idle) 
+        end 
+    end 
 	
 --[[ Toggle Engaged ( F9 ) or ( //gs c e ) ]]--	
   elseif command == 'e' then 
     if engaged == PDT then 
-	  engaged = MDT 
-	    if player.status == 'Engaged' then 
-		  equip(engaged) 
-		end 
-	elseif engaged == MDT then 
-	  engaged = BDT 
-	    if player.status == 'Engaged' then 
-		  equip(engaged) 
-		end 
-	elseif engaged == BDT then 
-	  engaged = DT 
-	    if player.status == 'Engaged' then 
-		  equip(engaged) 
-		end 
-	elseif engaged == DT then 
-	  engaged = DD 
-	    if player.status == 'Engaged' then 
-		  equip(engaged) 
-		end 
-	elseif engaged == DD then 
-	  engaged = PDT 
-	    if player.status == 'Engaged' then 
-		  equip(engaged) 
-		end 
+      engaged = MDT 
+        if player.status == 'Engaged' then 
+          equip(engaged) 
 	end 
+    elseif engaged == MDT then 
+      engaged = BDT 
+        if player.status == 'Engaged' then 
+          equip(engaged) 
+	end 
+    elseif engaged == BDT then 
+      engaged = DT 
+        if player.status == 'Engaged' then 
+	  equip(engaged) 
+	end 
+    elseif engaged == DT then 
+      engaged = DD 
+        if player.status == 'Engaged' then 
+	  equip(engaged) 
+	end 
+    elseif engaged == DD then 
+      engaged = PDT 
+        if player.status == 'Engaged' then 
+	  equip(engaged) 
+	end 
+    end 
   end
 end 
 
