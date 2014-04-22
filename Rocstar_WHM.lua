a95d8f74192580a48fe3bdda93e812f49417a8fa
Rocstar <john.east@techie.com>
2014-04-22 14:52:14 -0700

    Update Rocstar_WHM.lua

--- Rocstar_WHM.lua
+++ Rocstar_WHM.lua
@@ -2,8 +2,8 @@ function get_sets() pre = {} mid = {}
 
 --[[ Start Message ]]--
 
-  add_to_chat(200, 'Gearswap: Engaged DD ( ALT F9 ) Idle Refresh ( F9 )') 
-
+  add_to_chat(200, 'Gearswap: Idle Refresh ( F9 )') 
+  add_to_chat(200, 'Engaged DD ( ALT F9 ) ') 
 --[[ Change Macros ]]--
 
   send_command('@input /macro book 1;wait .1;input /macro set 1') 
@@ -35,17 +35,18 @@ function get_sets() pre = {} mid = {}
     neck="Orison Locket",ear1="Gifted Earring",
     ear2="Loquacious Earring",body="Marduk's Jubbah +1",
     hands="Gendewitha Gages",ring1="Prolix Ring",
-    ring2="Veneficium Ring",back="Swith Cape",
+    ring2="Veneficium Ring",back="Ogapepo Cape",
     waist="Witful Belt",legs="Orvail Pants +1",
     feet="Chelona Boots +1"} 
-
+	
+  pre.healing = {legs="Orison Pantaloons +2"} 
+  
 --[[ Cure Spellcasting ]]--
 
-  pre.Cure = {head="Cleric's Cap +2",neck="Aceso's Choker",
+  pre.cure = set_combine(pre.healing, {head="Cleric's Cap +2",neck="Aceso's Choker",
     body="Heka's Kalasiris",ring1="Prolix Ring",
     ring2="Veneficium Ring",back="Pahtli Cape",
-    waist="Capricornian Rope",legs="Nabu's Shalwar",
-    feet="Cure Clogs"} 
+    waist="Capricornian Rope",feet="Cure Clogs"}) 
 
 --[[ Weapon Skills ]]--
 
@@ -84,28 +85,22 @@ function get_sets() pre = {} mid = {}
   mid.Light = {waist='Korin Obi',back='Twilight Cape'} 
   mid.Dark = {waist='Anrin Obi',back='Twilight Cape'} 
 
---[[ Midcast Divine ]]--
+--[[ Midcast Nuke ]]--
 
-  mid.Divine = {} 
+  mid.nuke = {sub="Zuuxowu Grip",
+    ammo="Memoria Sachet",head="Buremte Hat",
+    neck="Quanpur Necklace",ear1="Friomisi Earring",
+    ear2="Hecate's Earring",body="Bokwus Robe",
+    hands="Otomi Gloves",ring1="Demon's Ring",
+    ring2="Demon's Ring",back="Toro Cape",
+    waist="Othila Sash",legs="Gendewitha Spats",
+    feet="Gendewitha Galoshes"}  
 
 --[[ MND Enfeeb ]]--
   mid.MND_Enfeeb = {} 
 
 --[[ INT Enfeeb ]]--
   mid.INT_Enfeeb = {} 
-
---[[ Midcast Elemental ]]--
-  mid.Elemental = {} 
-
---[[ Midcast Dark ]]--
-  mid.Dark_Magic = {main="Eminent Staff",sub="Zuuxowu Grip",
-    ammo="Memoria Sachet",head="Espial Cap",
-    neck="Orison Locket",ear1="Gifted Earring",
-    ear2="Loquacious Earring",body="Espial Gambison",
-    hands="Espial Cuffs",ring1="Prolix Ring",
-    ring2="Veneficium Ring",back="Swith Cape",
-    waist="Witful Belt",legs="Wayfarer Slops",
-    feet="Wayfarer Clogs"} 
 
 --[[ Cure Potency ]]--
   mid.Cure = {main="Arka IV",sub="Verse Strap +1",
@@ -151,7 +146,7 @@ function get_sets() pre = {} mid = {}
 --[[ Refresh ]]--
 
   REF = {main="Terra's Staff",sub="Oneiros Grip",
-    ammo="Incantor Stone",head="Wivre Hairpin",
+    ammo="Shadow Sachet",head="Wivre Hairpin",
     neck="Wiglen Gorget",ear1="Black Earring",
     ear2="Darkness Earring",body="Gendewitha Bliaut",
     hands="Serpentes Cuffs",ring1="Dark Ring",
@@ -161,8 +156,14 @@ function get_sets() pre = {} mid = {}
 	
 --[[ Regen ]]--
 
-  REG = {neck="Wiglen Gorget",ring1="Paguroidea Ring",
-    ring2="Sheltered Ring"} 
+  REG = {main="Terra's Staff",sub="Oneiros Grip",
+    ammo="Shadow Sachet",head="Wayfarer Circlet",
+    neck="Wiglen Gorget",ear1="Black Earring",
+    ear2="Darkness Earring",body="Wayfarer Robe",
+    hands="Wayfarer Cuffs",ring1="Paguroidea Ring",
+    ring2="Sheltered Ring",back="Cheviot Cape",
+    waist="Slipor Sash",legs="Wayfarer slops",
+    feet="Wayfarer Clogs"} 
 	
 --[[ Accuracy ]]--
 
@@ -198,31 +199,35 @@ function get_sets() pre = {} mid = {}
   
 --[[ Idle ]]--
 
-  idle = REF 
+  idle = REG 
 
 end 
 
 function precast(spell)
   if spell.skill == 'HealingMagic' then 
-    equip(pre.Cast, pre.Cure, pre[spell.element])
+    if windower.wc_match(spell.english, 'Cura*|Cure*') then  
+      equip(pre.cast, pre.cure, pre[spell.element]) 
+	else 
+	  equip(pre.cast, pre.healing, pre[spell.element]) 
+	end
   elseif spell.skill == 'EnhancingMagic' then 
     if spell.english == 'Stoneskin' then 
-      equip(pre.Cast, pre[spell.english], pre[spell.element]) 
+      equip(pre.cast, pre[spell.english], pre[spell.element]) 
     else 
-      equip(pre.Cast, pre.enhancing, pre[spell.element]) 
+      equip(pre.cast, pre.enhancing, pre[spell.element]) 
     end 
   elseif S{'WeaponSkill', 'JobAbility'}:contains(spell.type) then 
-    equip(pre[spell.english]) 
+    if pre[spell.english] then 
+      equip(pre[spell.english]) 
+	end
   else 
-    equip(pre.Cast, pre[spell.element]) 
+    equip(pre.cast, pre[spell.element]) 
   end 
 end 
 
 function midcast(spell) 
-  if spell.skill == 'DivineMagic' then 
-    equip(mid.Divine) 
-  elseif spell.skill == 'HealingMagic' then 
-    if windower.wc_match(spell.english, 'Cura*|Cure*') then  
+  if spell.skill == 'HealingMagic' then 
+    if windower.wc_match(spell.english, 'Cura*|Cure*') then 
       equip(mid.Cure)  
     elseif spell.english:endswith('na') and spell.name ~= 'Cursna' then 
       equip(mid.NA) 
@@ -257,10 +262,8 @@ function midcast(spell)
     else 
       equip(mid.MND_Enfeeb) 
     end 
-  elseif spell.skill == 'DarkMagic' then 
-    equip(mid.Dark_Magic) 
-  elseif spell.skill == 'ElementalMagic' then 
-    equip(mid.Elemental) 
+  elseif S{'DivineMagic', 'ElementalMagic', 'DarkMagic'}:contains(spell.skill) then 
+    equip(mid.nuke) 
   end
 end
 
@@ -286,15 +289,15 @@ function self_command(command)
 
 --[[ Toggle Idle ( ALT F9 ) or ( //gs c i ) ]]--		
   if command == 'i' then 
-    if idle == PDT then 
-      idle = MDT 
-      add_to_chat(200, 'Gearswap: idle now MDT') 
+    if idle == REF then 
+      idle = REG 
+      add_to_chat(200, 'Gearswap: Idle set to Regen') 
         if player.status ~= 'Engaged' then 
           equip(idle) 
 	end 
-    elseif idle == MDT then 
-      idle = PDT 
-      add_to_chat(200, 'Gearswap: idle now PDT') 
+    elseif idle == REG then 
+      idle = REF 
+      add_to_chat(200, 'Gearswap: Idle set to Refresh') 
         if player.status ~= 'Engaged' then 
           equip(idle) 
         end 
